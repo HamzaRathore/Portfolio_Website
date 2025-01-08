141610d77a0bb3857230755e2dd17602578b7fe9
HamzaRathore <128692254+HamzaRathore@users.noreply.github.com>
2025-01-08 22:46:21 +0500

    Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,37 +1,101 @@
-import React, { useEffect } from 'react'
-// import AnimatedSection from './AnimatedSection';
-import AOS from "aos";
-import "aos/dist/aos.css";
-const Contact = () => {
+import React from 'react'
+import gym from '../assets/portfolio/gym.png'
+import car from '../assets/portfolio/rent-car.png'
+import commerce from '../assets/portfolio/e-commerce.png'
+import grocery from '../assets/portfolio/Grocery.png'
+import baywatch from '../assets/portfolio/BayWatch.png'
+import weather from '../assets/portfolio/reactWeather.jpg'
+import AnimatedSection from './AnimatedSection'
 
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
-  }, []);
+const Portfolio = () => {
+    const portfolios = [
+        {
+            id: 1,
+            src: gym,
+            href: 'https://github.com/HamzaRathore/Gym_Website',
+        },
+        {
+            id: 2,
+            src: car,
+            href: 'https://github.com/HamzaRathore/Rent-a-car',
+            link: 'https://auto-rental-car.netlify.app/',
+        },
+        {
+            id: 3,
+            src: commerce,
+            href: 'https://github.com/HamzaRathore/e-commerce-redux-toolkit',
+            link: 'https://e-commerce-redux-toolkit.netlify.app/',
+        },
+        {
+            id: 4,
+            src: grocery,
+            href: 'https://github.com/HamzaRathore/Grocery-Store-Website',
+            link: 'https://online-grocery-store-website.netlify.app/',
+        },
+        {
+            id: 5,
+            src: baywatch,
+            href: 'https://github.com/HamzaRathore/BayWatch-movie-website',
+            link: 'https://baywatch.netlify.app/',
+        },
+        {
+            id: 6,
+            src: weather,
+            href: 'https://github.com/HamzaRathore',
+        },
+    ]
 
-  return (
-    <div name="contact" className='bg-gradient-to-b from-yellow-400 via-black to-gray-700 p-4 text-white pt-36'>
-       {/* <AnimatedSection direction="right"> */}
-      <div className='flex flex-col p-4 justify-center w-full mx-auto h-full' data-aos="fade-up"
-                data-aos-delay="200">
-        <div className='items-center justify-center flex flex-col '>
-            <p className='text-4xl font-bold inline border-b-4 border-gray-500 '>Contact</p>
-            <p className='py-6'>Submit the form to get in touch with me </p>
+    return (
+        <div
+            name="portfolio"
+            className="bg-gradient-to-b from-yellow-400 via-black to-gray-700 w-full text-white md:h-screen pt-20"
+        >
+            <AnimatedSection direction="right">
+                <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                    <div className="pb-8">
+                        <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                            Portfolio
+                        </p>
+                        <p className="py-6">Check Out some of my work right here</p>
+                    </div>
+                    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-14 sm:px-0 justify-center">
+                         {portfolios.map(({ id, src, href, link }) => (
+                            <div
+                                key={id}
+                                className="shadow-md shadow-gray-500 rounded-lg w-80   duration-300 hover:scale-105"
+                            >
+                                <img
+                                    src={src}
+                                    alt=""
+                                    className="rounded-md"
+                                />
+                                <div className="flex items-center justify-center">
+                                    <a
+                                        href={href}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="w-28 rounded-lg px-8 py-3 m-4 duration-200 hover:scale-105 hover:bg-gradient-to-r from-black to-gray-700 cursor-pointer hover:text-white"
+                                    >
+                                        Code
+                                    </a>
+                                    {link && (
+                                        <a
+                                            href={link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="w-28 rounded-lg px-8 py-3 m-4 duration-200 hover:scale-105 hover:bg-gradient-to-r from-black to-gray-700 cursor-pointer hover:text-white"
+                                        >
+                                            Demo
+                                        </a>
+                                    )}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </AnimatedSection>
         </div>
-        <div className='flex justify-center items-center'>
-            <form action="https://getform.io/f/allljrla" method='POST' className='flex flex-col w-full md:w-1/2'>
-                <input type="text" name='name' placeholder='Enter your name' required  className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none '/>
-                <input type="email" name='email' placeholder='Enter your email' required  className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none my-4 '/>
-                 <textarea name="message"placeholder='Enter Your Message' id="" rows="10" required className='p-2 border-2 bg-transparent rounded-md text-white focus:outline-none'></textarea>
-                 <button className='text-white bg-gradient-to-b from bg-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Lets Talk</button>
-            </form>
-        </div>
-      </div>
-      {/* </AnimatedSection> */}
-    </div>
-  )
+    )
 }
 
-export default Contact
+export default Portfolio
