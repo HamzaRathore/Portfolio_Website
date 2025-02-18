31b95b8fae26c5f78056be85152ac7bb33086598
HamzaRathore <128692254+HamzaRathore@users.noreply.github.com>
2025-02-18 19:06:19 +0500

    Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,6 +5,9 @@ import tailwind from "../assets/tailwind.png";
 import javascript from "../assets/javascript.png";
 import react from "../assets/react.png";
 import github from "../assets/github.png";
+import typeScript from "../assets/typescript.png";
+import Next from "../assets/next.png";
+import Firebase from "../assets/firebase.png";
 import AnimatedSection from './AnimatedSection';
 const Experience = () => {
   const techs = [
@@ -34,12 +37,30 @@ const Experience = () => {
     },
     {
       id: 5,
+      src: typeScript,
+      title: "TypeScript",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 6,
       src: react,
       title: "React",
       style: "shadow-blue-600",
     },
     {
-      id: 6,
+      id: 7,
+      src: Next,
+      title: "Next.Js",
+      style: "shadow-gray-600",
+    },
+    {
+      id: 8,
+      src: Firebase,
+      title: "Firebase",
+      style: "shadow-orange-600",
+    },
+    {
+      id: 9,
       src: github,
       title: "GitHub",
       style: "shadow-gray-400",
@@ -49,7 +70,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-700 via-black to-yellow-400 w-full h-screen pt-40 "
+      className="bg-gradient-to-b from-gray-700 via-black to-yellow-400 w-full h-screen pt-40 md:pt-24 "
     >
       <AnimatedSection direction="left">
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
@@ -60,7 +81,7 @@ const Experience = () => {
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0  ">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
@@ -68,8 +89,8 @@ const Experience = () => {
                 `shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`
               }
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4">{title}</p>
+              <img src={src} alt="" className=" w-14 md:w-20 lg:w-20  mx-auto" />
+              <p className="md:mt-4 mt-3">{title}</p>
             </div>
           ))}
         </div>
