2b1330af7e5f1adc74ea846a1bb006819667b154
HamzaRathore <128692254+HamzaRathore@users.noreply.github.com>
2025-04-15 18:56:37 +0500

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,11 @@ const Home = () => {
     <div name="home" className="h-screen w-full bg-gradient-to-b from-yellow-400 via-black to-gray-700 pt-28 md:pl-48 md:pt-52 lg:pt-36 overflow-hidden">
       <AnimatedSection direction="left">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-5 md:flex-row">
-          <div className="flex flex-col justify-center h-full  md:mr-16">
-            <h2 className="text-4xl sm:text-7xl font-bold md:text-5xl lg:text-7xl text-white">I'm a Front End Developer</h2>
-            <p className="text-gray-400 md:text-xl lg:text-3xl py-4 max-w-md">
-              Specializing in creating seamless, interactive user experiences with
-               CSS, Tailwind CSS, JavaScript, TypeScript, React and Next.Js.
+          <div className="flex-1 flex flex-col justify-center h-full md:mr-12 md:pr-4">
+            <h2 className="text-4xl lg:w-[550px] sm:text-7xl font-bold md:text-5xl lg:text-7xl text-white">I'm a MERN Stack Developer</h2>
+            <p className="text-gray-400 md:text-xl lg:text-2xl py-4 max-w-2xl">
+            I craft full-stack web applications using MongoDB, Express.js, React.js, and Node.js. 
+            Passionate about building dynamic user interfaces and powerful backend systems.
             </p>
             <div>
               <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-black to-gray-700 cursor-pointer">
